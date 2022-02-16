67df886d12c224887c8ea118d6abb68cb3c40c20
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-02-16 17:16:54 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.nytimes.com/"
+response = requests.get(url)
+
+print(response)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+# raw = (soup.find_all(class_="css-5n6ag4"))
+# for n in raw:
+#     print(n)
+# #
+
+user_input = str(input("What keyword would you like to search the New York Times by?\n").strip())
+raw = (soup.find_all("p"))
+for n in raw:
+    text = n.getText()
+    if user_input in text:
+        print(text)
+    else:
+        continue
+    print("NYT")
